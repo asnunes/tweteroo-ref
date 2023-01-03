95c477053b47b7abb2db2d9304133e5cfd828a4f
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2023-01-02 21:21:58 -0300

    chore: port config

--- src/server.js
+++ src/server.js
@@ -84,6 +84,7 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Running on PORT 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on PORT ${port}`);
 });
